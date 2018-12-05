ab1fa266425c7f65d194dfd51663aa426c1a52ea
Armon Raphiel <araphiel@gmail.com>
2018-12-05 00:35:38 -0600

    sample code for sample page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,16 +1,22 @@
+/* eslint-disable */
 /**
- * If you want page specific javascript, I'd recommend 
+ * If you want page specific javascript, I'd recommend
  * create a new file and adding to webpack as an entrypoint.
  * If you want things consolidate you can use DOM Based Routing (see the bodyClass function)
  * or use webpacks code-splitting (which can also work with the above)
 */
 
-// Note: You can use jQuery. See webpack.config.js line 81
+// Note: You can use jQuery. See webpack.config.js - line 81
 
-import 'bootstrap'
+
+import 'bootstrap';
 import { bodyClass } from './globals/helpers';
 
 if (bodyClass('sample')) {
-    const randomNum = x => x * Math.floor(Math.random())
-    alert(`This page is running compiled Javascript (ES${randomNum(5)})`)
+  const randomNum = x => x * (100 * Math.random());
+  const fun = document.querySelector('#fun')
+
+  document.addEventListener("DOMContentLoaded", () => {
+    fun.innerHTML = `This page is running compiled Javascript - ES${randomNum(5)})`;
+  })
 }
