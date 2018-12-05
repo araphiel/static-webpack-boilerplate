6ddfcdffbddd749f1420a5a04e1381aac6fbeb8f
Armon Raphiel <araphiel@gmail.com>
2018-12-05 00:53:26 -0600

    Sample code

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -13,10 +13,10 @@ import 'bootstrap';
 import { bodyClass } from './globals/helpers';
 
 if (bodyClass('sample')) {
-  const randomNum = x => x * (100 * Math.random());
-  const fun = document.querySelector('#fun')
-
+  let randomNum = x => x * Math.ceil(100 * Math.random()),
+      fun = document.querySelector('#fun');
+        
   document.addEventListener("DOMContentLoaded", () => {
-    fun.innerHTML = `This page is running compiled Javascript - ES${randomNum(5)})`;
+    setInterval(x => fun.innerHTML = `ES${randomNum(5)}`, 500)
   })
 }
